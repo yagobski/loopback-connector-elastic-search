113e6b163a8965b4fcf39bb4f4e03da5f7fbf8ab
Pulkit Singhal <pulkit@shoppinpal.com>
2015-03-19 15:35:43 -0500

    sample boot script to demo and sanity test the various ways to create a model

--- examples/server/boot/01-create-user.js
+++ examples/server/boot/01-create-user.js
@@ -1,24 +1,31 @@
-// DEBUG=boot:create-model-instances slc run
+/**
+ * To run:
+ *   DEBUG=loopback:connector:*,loopback:datasource,boot:test:* slc run
+ * or
+ *   DEBUG=loopback:connector:*,loopback:datasource,boot:test:* node server/server.js
+ */
+
 var debug = require('debug')('boot:test:01-create-user');
+var _ = require('lodash');
+var Promise = require('bluebird');
 
 module.exports = function(app) {
     var UserModel = app.models.UserModel;
 
-    // DEBUG=loopback:connector:*,loopback:datasource,boot:test:* node server/server.js
 
     var userWithStringId = {
         id: '1',
         realm: 'portal',
-        username: 'userWithId@shoppinpal.com',
-        email: 'userWithId@shoppinpal.com',
-        password: 'userWithId'
+        username: 'userWithStringId@shoppinpal.com',
+        email: 'userWithStringId@shoppinpal.com',
+        password: 'userWithStringId'
     };
     var userWithNumericId = {
-        id: 1,
+        id: 2,
         realm: 'portal',
-        username: 'userWithId@shoppinpal.com',
-        email: 'userWithId@shoppinpal.com',
-        password: 'userWithId'
+        username: 'userWithNumericId@shoppinpal.com',
+        email: 'userWithNumericId@shoppinpal.com',
+        password: 'userWithNumericId'
     };
     var userWithoutAnyId = {
         realm: 'portal',
@@ -27,8 +34,27 @@ module.exports = function(app) {
         password: 'userWithoutId'
     };
 
-    return UserModel.createAsync(userWithoutAnyId)
-        .then(function(resolvedData){
-            debug(JSON.stringify(resolvedData,null,2));
+    var users = [userWithStringId, userWithNumericId, userWithoutAnyId];
+
+    Promise.map(
+        users,
+        function (user) {
+            return UserModel.createAsync(user)
+                .then(function (resolvedData) {
+                    debug(JSON.stringify(resolvedData,null,2));
+                    return Promise.resolve();
+                },
+                function (err) {
+                    console.error(err);
+                    return Promise.reject();
+                });
+        },
+        {concurrency: 1}
+    )
+        .then(function () {
+            debug('all work for UserModels finished');
+        },
+        function (err) {
+            console.error(err);
         });
 };
