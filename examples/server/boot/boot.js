d88251cbf2d55486e89a5e3635cfe270e6616a44
Pulkit Singhal <pulkit@shoppinpal.com>
2015-03-21 13:17:13 -0500

    moving some boot scripts that help with sanity testing

--- examples/server/boot/boot.js
+++ examples/server/boot/boot.js
@@ -1,5 +1,8 @@
 module.exports = function(app) {
-    require('./../user/01-find-user.js')(app);
-    require('./../user/02-findOrCreate-user.js')(app);
-    //require('./../user/03-create-user.js')(app);
+    //require('./../users/01-find-user.js')(app);
+    //require('./../users/02-findOrCreate-user.js')(app);
+    //require('./../users/03-create-user.js')(app);
+
+    //require('./../roles/01-create-role.js')(app);
+    require('./../roles/02-findOrCreate-role.js')(app);
 };
