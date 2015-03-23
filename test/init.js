e18c2f4d842a790e91f04aa40e907476a897d501
Pulkit Singhal <pulkit@shoppinpal.com>
2015-03-23 10:13:40 -0500

    use `should` from `chai` library

--- test/init.js
+++ test/init.js
@@ -3,7 +3,7 @@
 var chai = require('chai');
 global.expect = chai.expect;
 global.assert = chai.assert;
-global.should = chai.should; // had to run `npm install --save-dev should` ... why didn't this suffice?
+global.should = chai.should(); // Why is the function being executed? Because the "should" interface extends Object.prototype to provide a single getter as the starting point for your language assertions.
 
 global._ = require('lodash');
 
